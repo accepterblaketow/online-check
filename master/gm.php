edc7ccbbbabd767e497a1f5a7cef3a8add38ef89
RoseNFan <48249004+accepterblaketow@users.noreply.github.com>
2021-12-30 12:51:38 +0800

    80%

--- master/gm.php
+++ master/gm.php
@@ -46,17 +46,9 @@
         <div id="t" style="background-color: #2828FF;display: block;height:70px;">
             <span style="float:right;"><a onclick="location.href='../login.php'">登出</a></span>
             <ul id="t" style="font-size: 0;position: absolute;top:25px">
-<<<<<<< HEAD:master/gm.php
                 <li><a href="dm.php">訂單管理</a></li>
                 <li><a href="rm.php">房型管理</a></li>
                 <li><a href="gm.php">財報圖表</a></li>
-=======
-                <li><a href="intro1.php">房型介紹</a></li>
-                <li><a href="res.php">預約訂房</a></li>
-                <li><a href="pinfo.php">會員資料</a></li>
-                <li><a href="q.php">預約查詢</a></li>
-                <li><a href="eva.php">客戶評價</a></li>
->>>>>>> b34d7e84bbe80c3098cce8d992db2329e6ca82fc:user/intro1.php
             </ul>            
         </div>
         <?php
